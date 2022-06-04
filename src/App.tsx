fbae4f8a7302d37cd7df26671bd16109dff56576
jeffhu1 <jeffhu@stanford.edu>
2022-06-04 00:47:46 -0700

    add formik to home search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
 import * as React from "react";
 
-import { ChakraProvider, Container, Input } from "@chakra-ui/react";
+import { Formik } from "formik";
+import {
+  Button,
+  ChakraProvider,
+  Container,
+  Input,
+  FormControl,
+  FormLabel,
+  VStack,
+} from "@chakra-ui/react";
 
 function App() {
   return (
     <ChakraProvider>
-      <Container>
-        <Input placeholder="tx hash" size="lg" mt={20} />
+      <Container mt={20}>
+        <Formik
+          initialValues={{ hash: "" }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({ values, isSubmitting, handleChange, handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <VStack spacing={4} align="flex-start">
+                <FormControl>
+                  <FormLabel htmlFor="hash">Transaction Hash</FormLabel>
+                  <Input
+                    name="hash"
+                    placeholder="Hash"
+                    type="text"
+                    size="lg"
+                    isDisabled={isSubmitting}
+                    onChange={handleChange}
+                    value={values.hash}
+                  />
+                </FormControl>
+                <Button type="submit" disabled={isSubmitting}>
+                  Submit
+                </Button>
+              </VStack>
+            </form>
+          )}
+        </Formik>
       </Container>
     </ChakraProvider>
   );
