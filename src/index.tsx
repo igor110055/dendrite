7a836d0e558e2f04eed9199edd6b74c8d46c9935
jeffhu1 <jeffhu@stanford.edu>
2022-06-04 21:01:11 -0700

    add routes

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        {/* <Route path="/" element={<Tx />} /> */}
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
 
